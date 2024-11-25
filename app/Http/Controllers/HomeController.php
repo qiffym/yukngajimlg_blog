40f6355c244e4b9432d83398743174be4d712bd4
qiffym <qiww.dev@gmail.com>
2024-11-25 20:44:21 +0700

    popularity

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums;
+use App\Http\Resources\ArticleBlockResource;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        return inertia('home');
+        return inertia('home', [
+            'popular_articles' => fn() => ArticleBlockResource::collection(
+                Article::query()
+                    ->select(['id', 'category_id', 'user_id', 'title', 'slug', 'thumbnail', 'teaser', 'published_at'])
+                    ->with(['category:id,name,slug', 'user:id,name'])
+                    ->whereStatus(Enums\ArticleStatus::Published)
+                    ->popularThisWeek()
+                    ->limit(3)
+                    ->get()
+            ),
+            'articles' => fn() => ArticleBlockResource::collection(
+                Article::query()
+                    ->select(['id', 'category_id', 'user_id', 'title', 'slug', 'thumbnail', 'teaser', 'published_at'])
+                    ->with(['category:id,name,slug', 'user:id,name'])
+                    ->where('status', Enums\ArticleStatus::Published)
+                    ->latest('published_at')
+                    ->limit(6)
+                    ->get()
+            )
+        ]);
     }
 }
--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -1,12 +1,52 @@
 import { AppLayout } from '@/layouts/app-layout';
 import { Container } from '@/components/container';
 import { Head } from '@inertiajs/react';
+import { CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { ArticleCard } from './articles/partials/article-card';
 
-export default function Home() {
+export default function Home(props) {
+    const { articles, popular_articles } = props;
+    console.log('🚀 ~ Home ~ popular_articles:', popular_articles);
+    console.log('🚀 ~ Home ~ articles:', articles);
+
     return (
         <div>
             <Head title="Home" />
-            <Container>Home page</Container>
+            <Container>
+                <div className="space-y-16 sm:space-y-24">
+                    <section id="popular">
+                        <CardHeader className="mb-6 p-0">
+                            <CardTitle>Popular Articles</CardTitle>
+                            <CardDescription>A collection of the most popular articles from our blog.</CardDescription>
+                        </CardHeader>
+                        {popular_articles.length > 0 ? (
+                            <div className="grid gap-x-12 gap-y-16 lg:grid-cols-3">
+                                {popular_articles.map((article) => (
+                                    <ArticleCard key={article.id} article={article} />
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No articles found.</p>
+                        )}
+                    </section>
+
+                    <section id="latest">
+                        <CardHeader className="mb-6 p-0">
+                            <CardTitle>Latest Articles</CardTitle>
+                            <CardDescription>A collection of the latest articles from our blog.</CardDescription>
+                        </CardHeader>
+                        {articles.length > 0 ? (
+                            <div className="grid gap-x-12 gap-y-16 lg:grid-cols-3">
+                                {articles.map((article) => (
+                                    <ArticleCard key={article.id} article={article} />
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No articles found.</p>
+                        )}
+                    </section>
+                </div>
+            </Container>
         </div>
     );
 }
